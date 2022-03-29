f1a3f5dad93e14b194fdbcef19a068a4aa6398e6
虚芜 <90039846+cccccccyyyyyyyyy@users.noreply.github.com>
2022-03-29 23:41:50 +0800

    Add files via upload
    这个模型用来将txt文件生成xml文件，是微改别人的，它是用于yolov3半自动打标的。它的优势是完善了xml的格式，不硬性要求txt文件和对应的image图片全部一一对应，它在中间增加了删除了单独的图片文件的代码。（改模型是中的txt文件用图片文件经过预测获得txt文件的）。还有一点小问题，就是里面的<truncated>0</truncated>这个部分我看了用labelimg打标出来的可能是1，目前不知道怎么改正。不过这样子的xml文件经过后期labelimg打标微调后用到后面的训练是没有问题的，它的logs文件是可以用来预测的。对于txt文件只需对predict文件和yolo文件进行微改就可以得到，有必要的时候我再上传。

---
+++ xcyxml.py
@@ -0,0 +1,90 @@
+#! /usr/bin/python
+# -*- coding:UTF-8 -*-
+import os
+txt_dir = 'txt_out'
+images_dir = 'VOCdevkit\VOC2007\JPEGImages'
+txts = []
+imgs = []
+for txt in os.listdir(txt_dir):
+    txts.append(txt.split('.')[0])
+
+for image_name in os.listdir(images_dir):
+    imgs.append(os.path.splitext(image_name)[0])
+    imgs.append(image_name.split('.')[0])
+    image_name = image_name.split('.')[0]
+    if image_name not in txts:
+        image_name = image_name + '.jpg'
+        # print(image_name)
+        os.remove(os.path.join(images_dir,image_name))
+        
+from operator import length_hint
+import os, sys
+import glob
+from PIL import Image
+
+# 图像存储位置
+src_img_dir = "VOCdevkit/VOC2007/JPEGImages"
+# 图像的 ground truth 的 txt 文件存放位置
+src_txt_dir = "txt_out"
+src_xml_dir = "VOCdevkit\VOC2007\Annotations"
+
+cy = src_img_dir.split('/') 
+t = len(cy)-1
+img_Lists = glob.glob(src_img_dir + '/*.jpg')
+
+img_basenames = [] # e.g. 100.jpg
+for item in img_Lists:
+    img_basenames.append(os.path.basename(item))
+
+img_names = [] # e.g. 100
+for item in img_basenames:
+    temp1, temp2 = os.path.splitext(item)
+    img_names.append(temp1)
+
+for img in img_names:
+    im = Image.open((src_img_dir + '/' + img + '.jpg'))
+    width, height = im.size
+
+    # open the crospronding txt file
+    gt = open(src_txt_dir + '/' + img + '.txt').read().splitlines()
+    #gt = open(src_txt_dir + '/gt_' + img + '.txt').read().splitlines()
+
+    # write in xml file
+    #os.mknod(src_xml_dir + '/' + img + '.xml')
+    cyy = src_img_dir+img
+    xml_file = open((src_xml_dir + '/' + img + '.xml'), 'w')
+    xml_file.write('<annotation>\n')
+    xml_file.write('    <folder>'+str(cy[t])+'</folder>\n')
+    xml_file.write('    <filename>' + str(img) + '.jpg' + '</filename>\n')
+    xml_file.write('    <path>'+str(cyy)+'</path>\n')
+    xml_file.write('    <source>\n')
+    xml_file.write('        <database>Unknown</database>\n')
+    xml_file.write('    </source>\n')
+    xml_file.write('    <size>\n')
+    xml_file.write('        <width>' + str(width) + '</width>\n')
+    xml_file.write('        <height>' + str(height) + '</height>\n')
+    xml_file.write('        <depth>3</depth>\n')
+    xml_file.write('    </size>\n')
+
+    # write the region of image on xml file
+    i = 1
+    for img_each_label in gt:
+        spt = img_each_label.split(' ') #这里如果txt里面是以逗号‘，’隔开的，那么就改为spt = img_each_label.split(',')。
+        #print(spt)
+        #spt打印['Images/s1.jpg', '391', '243', '428', '355', '426', '249', '468', '367', '']
+        imax = len(spt)
+        for i in range(0, imax-1 - 4, 4):
+            #print(str(spt[i + 1]))
+            xml_file.write('    <object>\n')
+            xml_file.write('        <name>'+str(spt[0])+'</name>\n')
+            xml_file.write('        <pose>Unspecified</pose>\n')
+            xml_file.write('        <truncated>0</truncated>\n')
+            xml_file.write('        <difficult>0</difficult>\n')
+            xml_file.write('        <bndbox>\n')
+            xml_file.write('            <xmin>' + str(spt[i+2]) + '</xmin>\n')
+            xml_file.write('            <ymin>' + str(spt[i+3]) + '</ymin>\n')
+            xml_file.write('            <xmax>' + str(spt[i+4]) + '</xmax>\n')
+            xml_file.write('            <ymax>' + str(spt[i+5]) + '</ymax>\n')
+            xml_file.write('        </bndbox>\n')
+            xml_file.write('    </object>\n')
+    xml_file.write('</annotation>')
